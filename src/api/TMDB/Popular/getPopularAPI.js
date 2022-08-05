cc41d94f910487f75f009f08e39d427e3f7f425c
kimjisu <kjs2504@gmail.com>
2022-08-06 01:37:34 +0900

    feat: movie search

---
+++ src/api/TMDB/Popular/getPopularAPI.js
@@ -0,0 +1,23 @@
+import TMDBServer from '../baseUrl';
+
+export const getSearchData = async (search, page) => {
+  try {
+    const res = await TMDBServer({
+      url: `search/movie`,
+      method: 'GET',
+      params: {
+        language: process.env.REACT_APP_TMDB_API_LANGUAGE,
+        query: search,
+        page: page === undefined ? 1 : page,
+      }
+    });
+    if (res.status === 200) {
+      return res.data;
+    } else {
+      const errData = await res.data;
+      throw errData;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
