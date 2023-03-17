25ca6f150426002cfa6298383da86348705fd1f0
sacultang <sacultang@gmail.com>
2023-03-17 14:18:16 +0900

    [RFT] MovieImage 상위 div 삭제

--- src/pages/movies/MoviePosterImg.tsx
+++ src/pages/movies/MoviePosterImg.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { Box } from '@mui/material';
 import { IMovieResult } from '../../types/movieType';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import CardSkeleton from '../../components/skeleton/CardSkeleton';
 import { Similrar } from '../../types/similarType';
-
 interface IProps {
   movie: IMovieResult | Similrar;
   handleNavi: (id: number, type: string) => void;
@@ -20,43 +18,23 @@ const MoviePosterImg = ({ movie, detailType, handleNavi }: IProps) => {
   return (
     <>
       {!imgLoading && <CardSkeleton />}
-      <Box
-        sx={{
-          borderRadius: '10px',
-          // minHeight: 300,
-          height: {
-            xs: 650,
-            sm: 650,
-            md: 550,
-            lg: 550,
-          },
-          maxHeight: { xl: 300 },
-          overflow: 'hidden',
+
+      <LazyLoadImage
+        src={
+          movie?.poster_path
+            ? `https://image.tmdb.org/t/p/w400/${movie?.poster_path}`
+            : 'https://media.discordapp.net/attachments/1014088216132988928/1016987090208182293/Vector.png?width=456&height=684'
+        }
+        alt={movie?.original_title || movie?.original_name || 'default Img'}
+        onLoad={onLoad}
+        onClick={() =>
+          handleNavi(movie.id, movie.media_type ? movie.media_type : detailType)
+        }
+        style={{
+          cursor: 'pointer',
           width: '100%',
         }}
-      >
-        <LazyLoadImage
-          src={
-            movie?.poster_path
-              ? `https://image.tmdb.org/t/p/w400/${movie?.poster_path}`
-              : 'https://media.discordapp.net/attachments/1014088216132988928/1016987090208182293/Vector.png?width=456&height=684'
-          }
-          alt={movie?.original_title || movie?.original_name || 'default Img'}
-          onLoad={onLoad}
-          onClick={() =>
-            handleNavi(
-              movie.id,
-              movie.media_type ? movie.media_type : detailType
-            )
-          }
-          style={{
-            // objectFit: 'contain',
-            cursor: 'pointer',
-            // width: '100%',
-            height: '100%',
-          }}
-        />
-      </Box>
+      />
     </>
   );
 };
