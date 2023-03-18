22eae0eec8f0d324b738130c99644f8bb3fd9616
sacultang <sacultang@gmail.com>
2023-03-18 14:11:52 +0900

    [ADD] myList 목록 삭제 추가

--- src/components/Common/DrawerMenu.tsx
+++ src/components/Common/DrawerMenu.tsx
@@ -9,10 +9,11 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
 import Toolbar from '@mui/material/Toolbar';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeleteForeverOutlined from '@mui/icons-material/DeleteForeverOutlined';
 import SearchInput from '../../pages/home/SearchInput';
 import {
   drawerWidth,
@@ -20,7 +21,8 @@ import {
   tvPath,
   myFavoritePage,
 } from './DrawerMenuList';
-import { theme } from '../../theme';
+import { collection, deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 type NavStyleType = {
   isActive: boolean;
@@ -31,6 +33,7 @@ const buttonHandler = ({ isActive }: NavStyleType) => {
     backgroundColor: isActive ? '#f3f3f3' : '',
     borderLeft: isActive ? '5px solid var(--yellow-text-color)' : '',
     color: '#000 ',
+    display: 'flex',
   };
 };
 
@@ -40,12 +43,25 @@ interface DrawerMenuProp {
 }
 
 const DrawerMenu = ({ open, setOpen }: DrawerMenuProp) => {
-  const { pathname } = useLocation();
   const user = useSelector((state: RootState) => state.user.user);
   const myListPage = useSelector((state: RootState) => state.listMovie.list);
-
+  const navigate = useNavigate();
   const handlMenuOpenClose = () => {
     setOpen(false);
+  };
+  const handleDeleteMyList = async (
+    e: React.MouseEvent<HTMLDivElement>,
+    listId: string
+  ) => {
+    e.preventDefault();
+    const confirmResult = window.confirm('삭제 하시겠습니까?');
+    if (confirmResult) {
+      const docRef = doc(db, 'users', user.email as string);
+      const myListRef = collection(docRef, 'list');
+      const listDocRef = doc(myListRef, listId);
+      await deleteDoc(listDocRef);
+      navigate('/');
+    }
   };
   return (
     <Drawer
@@ -111,17 +127,7 @@ const DrawerMenu = ({ open, setOpen }: DrawerMenuProp) => {
           <ListItem key={list.text} disablePadding onClick={handlMenuOpenClose}>
             <NavLink to={`${list.path}`} style={buttonHandler}>
               <ListItemButton>
-                <ListItemText
-                  primary={list.text}
-                  sx={{
-                    color:
-                      `${theme.palette.mode}` === 'dark'
-                        ? `${pathname}` === `${list.path}`
-                          ? 'primary.main'
-                          : 'primary.light'
-                        : 'primary.main',
-                  }}
-                />
+                <ListItemText primary={list.text} />
               </ListItemButton>
             </NavLink>
           </ListItem>
@@ -144,17 +150,7 @@ const DrawerMenu = ({ open, setOpen }: DrawerMenuProp) => {
           <ListItem key={list.text} disablePadding onClick={handlMenuOpenClose}>
             <NavLink to={`${list.path}`} style={buttonHandler}>
               <ListItemButton>
-                <ListItemText
-                  primary={list.text}
-                  sx={{
-                    color:
-                      `${theme.palette.mode}` === 'dark'
-                        ? `${pathname}` === `${list.path}`
-                          ? 'primary.main'
-                          : 'primary.light'
-                        : 'primary.main',
-                  }}
-                />
+                <ListItemText primary={list.text} />
               </ListItemButton>
             </NavLink>
           </ListItem>
@@ -182,41 +178,27 @@ const DrawerMenu = ({ open, setOpen }: DrawerMenuProp) => {
             >
               <NavLink to={`${list.path}`} style={buttonHandler}>
                 <ListItemButton>
-                  <ListItemText
-                    primary={list.text}
-                    sx={{
-                      color:
-                        `${theme.palette.mode}` === 'dark'
-                          ? `${pathname}` === `${list.path}`
-                            ? 'primary.main'
-                            : 'primary.light'
-                          : 'primary.main',
-                    }}
-                  />
+                  <ListItemText primary={list.text} />
                 </ListItemButton>
               </NavLink>
             </ListItem>
           ))}
-          {myListPage.length &&
+          {myListPage &&
             myListPage.map((list) => (
-              <ListItem
-                key={list.id}
-                disablePadding
-                onClick={handlMenuOpenClose}
-              >
-                <NavLink to={`/list/${list.id}`} style={buttonHandler}>
-                  <ListItemButton>
-                    <ListItemText
-                      primary={decodeURIComponent(list.id)}
-                      sx={{
-                        color:
-                          `${theme.palette.mode}` === 'dark'
-                            ? `${pathname}` === `/list/${list.id}`
-                              ? 'primary.main'
-                              : 'primary.light'
-                            : 'primary.main',
-                      }}
-                    />
+              <ListItem key={list.id} disablePadding>
+                <NavLink
+                  to={`/list/${list.id}`}
+                  style={buttonHandler}
+                  onClick={handlMenuOpenClose}
+                >
+                  <ListItemButton sx={{ flex: 3 }}>
+                    <ListItemText primary={decodeURIComponent(list.id)} />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ flex: 1 }}
+                    onClick={(e) => handleDeleteMyList(e, list.id)}
+                  >
+                    <DeleteForeverOutlined color="info" />
                   </ListItemButton>
                 </NavLink>
               </ListItem>
