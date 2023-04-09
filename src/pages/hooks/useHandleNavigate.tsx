cf6e6b04de54f6e046d208f9a6b62265e15c8236
sacultang <sacultang@gmail.com>
2023-04-09 23:42:29 +0900

    [FIX] navigate의 state삭제 navigate의 state로 type,id 전달시 주소창에 주소를 직접 입력하면 state가 null이 되는 issue

--- src/pages/hooks/useHandleNavigate.tsx
+++ src/pages/hooks/useHandleNavigate.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { HandleClickNaviType } from '../../types/handleClickNaviType';
+
 const useHandleNavigate = () => {
   const navigate = useNavigate();
   const handleClickNavigate: HandleClickNaviType = (id, type) => {
-    navigate(`/details/${type}/${id}`, { state: { type, id } });
+    navigate(`/details/${type}/${id}`);
   };
   return handleClickNavigate;
 };
