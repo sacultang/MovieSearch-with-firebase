7745115eb69f10c17c28a124dcdd9dd96cb8618a
sacultang <sacultang@gmail.com>
2023-04-09 23:40:56 +0900

    [RFT] useHandleNavigate로 대체

--- src/pages/favorite/Favorite.tsx
+++ src/pages/favorite/Favorite.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import PageTitle from '../../components/common/PageTitle';
@@ -7,19 +7,16 @@ import Loader from '../../components/common/Loader';
 import GridItemProvider from '../../components/common/GridItemProvider';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import { HandleClickNaviType } from '../../types/handleClickNaviType';
+import useHandleNavigate from '../hooks/useHandleNavigate';
 const MovieCard = lazy(() => import('../movies/MovieCard'));
 
 const Favorite = () => {
   const location = useLocation();
   const params = useParams();
-  const navigate = useNavigate();
   const movieDatas = useSelector(
     (state: RootState) => state.favorite.favoriteMovie
   );
-  const handleClickNavigate: HandleClickNaviType = (id, type) => {
-    navigate(`/details/${type}/${id}`, { state: { type, id } });
-  };
+  const handleClickNavigate = useHandleNavigate();
   return (
     <Container sx={{ flexGrow: 1, minHeight: 800 }}>
       <PageTitle url={location.pathname} params={params} />
--- src/pages/favorite/ListPage.tsx
+++ src/pages/favorite/ListPage.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import PageTitle from '../../components/common/PageTitle';
 import Loader from '../../components/common/Loader';
 import GridItemProvider from '../../components/common/GridItemProvider';
@@ -7,17 +7,14 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import { HandleClickNaviType } from '../../types/handleClickNaviType';
+import useHandleNavigate from '../hooks/useHandleNavigate';
 const MovieCard = lazy(() => import('../movies/MovieCard'));
 
 const ListPage = () => {
   const location = useLocation();
   const params = useParams();
-  const navigate = useNavigate();
   const myList = useSelector((state: RootState) => state.listMovie.list);
-  const handleClickNavigate: HandleClickNaviType = (id, type) => {
-    navigate(`/details/${type}/${id}`, { state: { type, id } });
-  };
+  const handleClickNavigate = useHandleNavigate();
 
   return (
     <Container sx={{ flexGrow: 1, minHeight: 800 }}>
