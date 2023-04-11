f475927dfbb3b4c55fd67e54d6aa4b6532df745a
sacultang <sacultang@gmail.com>
2023-04-11 21:07:00 +0900

    [RFT] Footer 하단 사이즈 조정

--- src/components/common/AppBar.tsx
+++ src/components/common/AppBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,14 +7,18 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import DrawerMenu from './DrawerMenu';
 import RegisterGroup from '../RegisterGroup';
-
+import Footer from './Footer';
+import styled from '@emotion/styled';
 export default function PermanentDrawerLeft() {
   const [barOpen, setBarOpen] = useState(false);
 
   const handleDrawerOpen = () => {
+    console.log('open click');
     setBarOpen(true);
   };
-
+  useEffect(() => {
+    console.log(barOpen);
+  }, [barOpen]);
   return (
     <>
       <AppBar position="static">
@@ -25,26 +29,32 @@ export default function PermanentDrawerLeft() {
           <RegisterGroup />
         </Toolbar>
       </AppBar>
-      <Container
-        component="main"
-        sx={{
-          flex: 1,
-          p: 2,
-          pt: 9,
-          boxSizing: 'border-box',
-          width: {
-            xs: '485px',
-            sm: '600px',
-            md: '900px',
-            lg: '1200px',
-            xl: '1536px',
-          },
-          height: 'auto',
-        }}
-      >
+      <MainContainer component="main">
         <Outlet />
-      </Container>
+      </MainContainer>
+      <Footer />
       <DrawerMenu barOpen={barOpen} setBarOpen={setBarOpen} />
     </>
   );
 }
+
+type MainContainerProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> & {
+  component?: React.ElementType;
+};
+
+const MainContainer = styled(Container)<MainContainerProps>`
+  flex: 1;
+  padding: 2;
+  box-sizing: 'border-box';
+  width: {
+    xs: '485px';
+    sm: '600px';
+    md: '900px';
+    lg: '1200px';
+    xl: '1536px';
+  }
+  height: 'auto';
+`;
