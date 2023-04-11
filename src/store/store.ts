84de8a340aca328e9dee00b4974737abc6e2942b
sacultang <sacultang@gmail.com>
2023-04-11 21:56:30 +0900

    [DEL] barOpen slice 삭제 component state로 관리

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,7 +5,6 @@ import movieSlice from './movieSlice';
 import favoriteListSlice from './favoriteListSlice';
 import toastSlice from './toastSlice';
 import listMovieSlice from './listMovieSlice';
-import barOpenCloseSlice from './barOpenCloseSlice';
 export const store = configureStore({
   reducer: {
     user: userSlice,
@@ -14,7 +13,6 @@ export const store = configureStore({
     favorite: favoriteListSlice,
     toast: toastSlice,
     listMovie: listMovieSlice,
-    barOpen: barOpenCloseSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
