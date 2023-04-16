caeabafb8ea59188a09eb927c3ca006ab796124b
sacultang <sacultang@gmail.com>
2023-04-16 23:32:34 +0900

    [ADD] SkeletonCard 컴포넌트 작성

---
+++ src/components/SkeletonCard.tsx
@@ -0,0 +1,39 @@
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import styled from '@emotion/styled';
+interface SkeletonCardProp {
+  scrollcard?: string;
+  credit?: string;
+  cardWidth?: number;
+}
+const SkeletonCard = ({ scrollcard, credit, cardWidth }: SkeletonCardProp) => {
+  return (
+    <CardItem scrollcard={scrollcard}>
+      <Box
+        overflow="hidden"
+        height={scrollcard ? 300 : `${cardWidth && (cardWidth * 3) / 2}px`}
+      >
+        <Skeleton height="100%" variant="rounded" width="100%" />
+      </Box>
+      <Box height={credit ? 45 : 68}>
+        {!credit && (
+          <Box overflow="hidden" p={1}>
+            <Skeleton height={15} variant="rounded" />
+          </Box>
+        )}
+
+        <Box overflow="hidden" p={1}>
+          <Skeleton height={20} variant="rounded" />
+        </Box>
+      </Box>
+    </CardItem>
+  );
+};
+
+export default SkeletonCard;
+const CardItem = styled(Card)<{ scrollcard: string | undefined }>`
+  margin: 5px;
+  position: relative;
+  width: ${({ scrollcard }) => scrollcard && '200px'};
+`;
